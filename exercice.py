ebe1424df55d2ba76b9b87a1563e7aad807b021d
Maxime Pageot <maxime.pageot.313@gmail.com>
2021-11-02 12:21:26 -0400

    fait tous les exercices sauf le 1

--- exercice.py
+++ exercice.py
@@ -7,9 +7,56 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 
 # TODO: Définissez vos fonction ici
+def tripleEspace(a:str):
+    result = ""
+    for l in a:
+        if l == " ":
+            result += "   "
+        else:
+            result += l
+    return result
+
+def tripleEspaceFile(source:str):
+    sourceText = []
+    with open(source,"r") as file:
+        for l in file:
+            sourceText.append(str(l))
+    with open(source,"w") as file:
+        for l in sourceText:
+            file.write(tripleEspace(l))
+
+notesToLettres = {
+    (0,20):" F",
+    (21,30):" E",
+    (31,40):" D",
+    (41,60):" C",
+    (61, 80): " B",
+    (81, 90): " A",
+    (91, 100): " A*",
+
+}
+def gradation(n:int,table):
+    print(n)
+    toCheck = list(table.keys())
+    lettre = ""
+    for a in toCheck:
+        if a[0]<=n<=a[1]:
+            lettre = table[a]
+            break
+    return(str(n) + lettre)
+
+def gradationFile(source:str,table):
+    sourceText = []
+    with open(source,"r") as file:
+        for a in file:
+            sourceText.append(int(a))
+    print(sourceText)
+    with open("source"+" mentions","w") as file:
+        for a in sourceText:
+            file.write(gradation(a, table))
+            file.write("\n")
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
     pass
--- recettes.py
+++ recettes.py
@@ -4,9 +4,17 @@ def add_recipes(dictionnaire):
     ingrediants = input("Entrez la liste d'ingrédiants de la recette, svp séparer les ingrédiants par une ,\n").split(",")
     dictionnaire[name] = ingrediants
 
-    return dictionnaire
+def add_recipes_file(source):
+    with open(source,"a") as file:
+        file.write(input("Entrez le nom de votre recette?\n") + ":" + input("Entrez la liste d'ingrédiants de la recette, svp séparer les ingrédiants par une ,\n")+ "\n")
 
-
+def reconstituerDict(source):
+    result = {}
+    with open(source,"r") as file:
+        for a in file:
+            ingredientlist = a.split(":")[1].replace("\n","").split(",")
+            result[a.split(":")[0]] = ingredientlist
+    return(result)
 def print_recipe(ingredients: dict) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
     name = input("Entrez le nom d'une recette?\n")
@@ -16,3 +24,5 @@ def print_recipe(ingredients: dict) -> None:
     else:
         print("La recette demandée n'existe pas!")
         print(f"Les recettes existantes sont: {list(ingredients.keys())}")
+
+print (reconstituerDict("recettesListe"))
